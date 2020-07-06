87426cdac055c45ca88584c8a268f3bdb961053e
Joaquin Guardado <joaquin_guardado@hotmail.com>
2020-07-06 20:06:30 +0200

    Completed fine tunning layout

--- app/containers/CameraScreen.js
+++ app/containers/CameraScreen.js
@@ -5,9 +5,13 @@ import {
   Text,
   TouchableOpacity,
   Image,
+  Dimensions,
   ImageBackground,
 } from 'react-native';
 import {images, colors} from '../themes';
+
+const windowWidth = Dimensions.get('window').width;
+console.log('windowWidth', windowWidth);
 
 class CameraScreen extends Component {
   constructor(props) {
@@ -18,11 +22,18 @@ class CameraScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
-          <ImageBackground source={images.ripple} style={styles.image}>
-            <Text style={styles.buttonText}>TAKE PHOTO</Text>
-          </ImageBackground>
-        </TouchableOpacity>
+        {/* Menu Component */}
+        <View style={styles.menu}>
+          <Text style={styles.menuText}>Library</Text>
+        </View>
+        <View style={styles.centeredContent}>
+          {/* Button Component */}
+          <TouchableOpacity style={styles.button}>
+            <ImageBackground source={images.ripple} style={styles.image}>
+              <Text style={styles.buttonText}>TAKE PHOTO</Text>
+            </ImageBackground>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -31,9 +42,12 @@ class CameraScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.icon,
+  },
+  centeredContent: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.icon,
   },
   image: {
     flex: 1,
@@ -42,8 +56,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   button: {
-    width: 300,
-    height: 270,
+    width: windowWidth - 32,
+    height: windowWidth - windowWidth * 0.19,
   },
   buttonText: {
     paddingTop: 32,
@@ -51,6 +65,18 @@ const styles = StyleSheet.create({
     fontFamily: 'System',
     fontSize: 26,
     fontWeight: 'bold',
+  },
+  menu: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingTop: 32,
+    paddingHorizontal: 16,
+  },
+  menuText: {
+    color: colors.white,
+    fontFamily: 'System',
+    fontSize: 20,
+    fontWeight: '400',
   },
 });
 
